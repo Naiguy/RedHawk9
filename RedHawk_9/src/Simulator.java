ace35d1bdd389f53e15572ea07c6d862307a2ec7
wwermager <wwermager09@winona.edu>
2017-11-03 12:10:57 -0500

    added necessary parameters to process class and tested

--- RedHawk_9/src/Simulator.java
+++ RedHawk_9/src/Simulator.java
@@ -4,7 +4,15 @@ public class Simulator {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Process p1 = new Process();
+		p1.newProc(72,false,2,4,4);
 
+
+		System.out.println("Process p1 has a PID of: " + p1.getPid());
+		System.out.println("Has a CS? " + (p1.getCS() ? "Yes" : "No"));
+		System.out.println("Memory Requirement: " + p1.getMemReq());
+		System.out.println("CPU Burst of: " + p1.getCPUBurst());
+		System.out.println("IO Burst of: " + p1.getIOBurst());
 	}
 
 }

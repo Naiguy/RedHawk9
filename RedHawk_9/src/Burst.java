7a1a12f4d69b3670cefa54eca839be8f84426033
Naiguy <naiscalemail@gmail.com>
2017-11-13 17:25:44 -0600

    -a

--- RedHawk_9/src/Burst.java
+++ RedHawk_9/src/Burst.java
@@ -4,11 +4,18 @@ public class Burst
 	int preCST;
 	int criticalSection;
 	int postCST;
+	int totalTime;
 	public Burst(BurstType type, int preCSTime, int cs, int postCSTime)
 	{
 		bt = type;
 		preCST = preCSTime;
 		criticalSection = cs;
 		postCST = postCSTime;
+		totalTime = preCSTime + cs + postCSTime;	
+	}
+	public Burst(BurstType type, int time)
+	{
+		bt = type;
+		totalTime = time;
 	}
 }

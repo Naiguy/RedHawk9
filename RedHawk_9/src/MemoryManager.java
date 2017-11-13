ced1f2279e2913e95e720494040e1bb37506b38c
Naiguy <naiscalemail@gmail.com>
2017-11-13 17:05:08 -0600

    -a

--- RedHawk_9/src/MemoryManager.java
+++ RedHawk_9/src/MemoryManager.java
@@ -1,6 +1,9 @@
 public class MemoryManager {
 
 	static byte [][] PhysicalMemory = new byte [2][1024];
+	static byte [][] LogicalMemory = new byte [2][1024];
+	//static byte [][] PageRef = new byte[1][]
+	
 	
 	public void allocateMemory(int n) {
 		//n = needed memory

556dc5696a4fe8b2d528dd8d7707d7ec3af8d7f7
Naiguy <naiscalemail@gmail.com>
2017-11-13 16:47:42 -0600

    lol

--- RedHawk_9/src/MemoryManager.java
+++ RedHawk_9/src/MemoryManager.java
@@ -1,4 +1,15 @@
 
 public class MemoryManager {
 
+	static byte [][] PhysicalMemory = new byte [2][1024];
+	
+	public void allocateMemory(int n) {
+		//n = needed memory
+		
+	}
+	public void createLocalMemory() {
+		
+		
+	}
+	//each process would create a 
 }

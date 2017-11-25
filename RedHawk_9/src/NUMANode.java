e5274fbd42c7ac839485902159a60164173d9a04
Naiguy <naiscalemail@gmail.com>
2017-11-25 13:33:27 -0600

    NUMANode V1

--- RedHawk_9/src/NUMANode.java
+++ RedHawk_9/src/NUMANode.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 
 public class NUMANode {
 	//each NUMA Node
@@ -7,5 +8,58 @@ public class NUMANode {
 	//There's home node that contains a directory (we can put that in the
 	//memory manager
 	// fjsdkfjdslkfjsdklfsd
+	
+	//Alg
+	
+	//Numa nodes are like houses on the same block.
+	//ideally, you do everything on the same block, but you CAN travel.
+	//
+	
+	// 4 nodes of 128
+	
+	//
+	
+	public int totalMemory;
+	public int memoryUsed;
+	ArrayList<Process> procs = new ArrayList<Process>();
+	
+	NUMANode(int mem) {
+		memoryUsed = 0;
+		totalMemory = mem;
+	}
+	
+	public void useMem(Process proc) {
+		if (canFit(proc.getMemReq())) {
+			procs.add(proc);
+			memoryUsed = memoryUsed - proc.getMemReq();
+		}
+		else {
+			//do use alg to send elsewhere
+		}
+	}
+	public void releaseMem(Process proc) {
+		procs.remove(proc);
+		memoryUsed = memoryUsed + proc.getMemReq();
+	}
+	
+	public boolean canFit(int AmttoFit) {
+		int n = memoryUsed + AmttoFit;
+		if (n >= totalMemory) return false;
+		else return true;
+	}
+	
+	public boolean isFull() {
+		if (totalMemory == memoryUsed) return true;
+		else return false;
+	}
+
+	
+	
+	
+	
+	
+	
+	
+	
 	
 }
